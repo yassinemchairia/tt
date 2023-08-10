52d8055537f7924cc93ca54d960e0daff5dd07ca
yassinemchairia <115500157+yassinemchairia@users.noreply.github.com>
2023-08-10 17:00:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const pdfIframe = document.getElementById('pdf-iframe');
 
   // Remplacez l'URL par l'URL réelle de votre fichier PDF hébergé
-  const pdfUrl = 'URL_DU_FICHIER_PDF';
+  const pdfUrl = 'https://github.com/yassinemchairia/tt/blob/main/ticket44.pdf';
 
   // Charge le PDF dans l'iframe
   pdfIframe.src = pdfUrl;
